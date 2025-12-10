20e309563f6871d6b5ba264cbac053c25d99d50c
artefiq <taufiqhidayat140504@gmail.com>
2025-12-10 09:33:39 +0700

    fix: add token expiry checking

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
 import TransactionInput from '../components/TransactionInput.vue'
+import { isTokenValid } from '../utils/auth';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,16 +18,20 @@ const router = createRouter({
       component: RegisterView
     },
     {
-      path: '/',
-      name: 'home',
-      component: TransactionInput,
-      // Guard: Cek apakah user sudah login
-      beforeEnter: (to, from, next) => {
-        const token = localStorage.getItem('fintrack_token');
-        if (!token) next('/login');
-        else next();
+    path: '/',
+    name: 'home',
+    component: TransactionInput,
+    beforeEnter: (to, from, next) => {
+      const token = localStorage.getItem('fintrack_token');
+      
+      if (isTokenValid(token)) {
+        next(); // Lanjut masuk
+      } else {
+        localStorage.removeItem('fintrack_token'); 
+        next('/login'); 
       }
     }
+  }
   ]
 })
 
